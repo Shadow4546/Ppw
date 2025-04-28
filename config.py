7b75d5f37013ac72f1fb5c2e7341c7b13d991fbd
Shadow4546 <89007456+Shadow4546@users.noreply.github.com>
2025-04-28 17:24:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = "24473318"
 api_hash = "e7dd0576c5ac0ff8f90971d6bb04c8f5"
-bot_token = "BOT_TOKEN"
+bot_token = "7395800395:AAFEKnTiDmjKUQKrduvwdd5GhURroYDgo-A"
 auth_users = [7170328188,2051726263]
